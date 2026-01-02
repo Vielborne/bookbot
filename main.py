675ba7b14a496c06e623cf992f41e825b83f7d95
Vielborne <mbilaldogar07@gmail.com>
2026-01-02 09:40:55 -0600

    feat: add get_num_words and print book word total

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def get_book_text(file_path):
     return file_contents
 
 
+def get_num_words(book_text):
+    num_words = len(book_text.split())
+    return num_words
+
+
 def main():
     file_path = "books/frankenstein.txt"
     file_contents = get_book_text(file_path)
-    print(file_contents)
+    print(f"Found {get_num_words(file_contents)} total words")
 
 
 main()
