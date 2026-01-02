13802d268c4fbb99a96ef48890ad4ae41fd42a02
Vielborne <mbilaldogar07@gmail.com>
2026-01-02 10:17:29 -0600

    feat: add sort_character_list and sort_on to print detailed sorted character report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_num_characters, get_num_words
+import sys
+
+from stats import get_num_characters, get_num_words, sort_character_list
 
 
 def get_book_text(file_path):
@@ -8,10 +10,27 @@ def get_book_text(file_path):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
-    file_contents = get_book_text(file_path)
-    print(f"Found {get_num_words(file_contents)} total words")
-    print(get_num_characters(file_contents))
+    if len(sys.argv) != 1:
+        file_path = sys.argv[1]
+        file_contents = get_book_text(file_path)
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count ----------")
+
+        print(f"Found {get_num_words(file_contents)} total words")
+
+        print("--------- Character Count -------")
+
+        character_dict = get_num_characters(file_contents)
+        character_list = sort_character_list(character_dict)
+        for dict in character_list:
+            print(f"{dict['char']}: {dict['num']}")
+
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,18 @@ def get_num_characters(book_text):
             else:
                 character_dict[ch] = 1
     return character_dict
+
+
+def sort_character_list(character_dict):
+    character_list = []
+    for key in character_dict:
+        new_character_dict = {}
+        new_character_dict["char"] = key
+        new_character_dict["num"] = character_dict[key]
+        character_list.append(new_character_dict)
+        character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+
+def sort_on(character_list):
+    return character_list["num"]
